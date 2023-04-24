e497d90f3a9b981eab9afaa30208fa7bd4913657
Sashank Srinath <105050513+SashankSrinath@users.noreply.github.com>
2023-04-24 10:12:10 +0200

    Updating file to check taylor series prediction

--- examples/msp430x/large/msp430fr5994/launchpad/bench_dnn_resurrect/generate_time_lookup.py
+++ examples/msp430x/large/msp430fr5994/launchpad/bench_dnn_resurrect/generate_time_lookup.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-v1 = np.arange(1.8,2.85, 0.05) # Considering a precision of 50mV from 1800mv to 2800mV
+v1 = np.arange(1792,2817, 64) # Considering a precision of 50mV from 1800mv to 2800mV
 print(len(v1))
-v2 = np.arange(1.8,2.85, 0.05)
+v2 = np.arange(1792,2817, 64)
 t = np.arange(0,300,1)
 slope = np.zeros((len(v1), len(v2)))
 intercept = np.zeros((len(v1), len(v2)))
 
-x = np.zeros((len(v1), len(v2)))
+x1 = np.zeros((len(v1), len(v2)))
+x_approx = np.zeros((len(v1), len(v2)))
 
 k = 0
 for i in range(len(v1)):
@@ -18,31 +19,44 @@ for i in range(len(v1)):
             slope[i][j] = (v2[j] - v1[i]) / 10 # Considering that a measurement is taken with a time difference of 10s
             intercept[i][j] = v2[j] - slope[i][j] * 10
             # print(v1[i], ":", v2[j], "::", slope[i][j], "::", intercept[i][j])
-            plt.figure(1)
-            plt.plot(t, slope[i][j] * t + intercept[i][j])
+            # plt.figure(1)
+            # plt.plot(t, slope[i][j] * t + intercept[i][j])
             
-            plt.figure(2)
-            y = v1[i] + (3.3 * (1 - np.exp(-t * slope[i][j] / 3.5)))
-            plt.plot(t, y)
+            # plt.figure(2)
+            # y = v1[i] + (3300 * (1 - np.exp(-t * slope[i][j] / 3500)))
+            # plt.plot(t, y)
 
 # calculate the time at which y=3.3
-y = 3.3
-print("--------------------")
-print("  v1  |  v2  ||   t ")
-print("--------------------")
+y = 3300
+print("----------------------------")
+print("  v1  |  v2  ||  t  ||t_appx")
+print("----------------------------")
 for i in range(len(v1)):
     for j in range(len(v2)):
         if v1[i] < v2[j]:
-            x[i][j] = (-3.5 / slope[i][j]) * np.log(1 - (y - v1[i]) / 3.3)
-            print(" {} | {} || {} ".format("{:.2f}".format(v1[i]), "{:.2f}".format(v2[j]),int(x[i][j])))
+            
+            factor = 1 - ((3300 - v1[i])/3300) # values from 0.5 to 0.83
+            # print(factor)
+            coeff = np.log(factor)  # values from -0.18 to -0.61
+            x1[i][j] = (-3500 / slope[i][j]) * coeff
+            
+            factor2 = ((v1[i]-3300)/3300)
+            x = factor2
+            # coeff2 =-2.486193160 + (5.951925226 + (-6.570618861 + (4.252664361 - 1.149089989*factor2)*factor2)*factor2)*factor2 #(factor2) - (factor2)**2/2 #+ (factor2)**3/3 # - (factor2)**4/4
+            coeff2 = factor2 - (factor2*factor2)/2
+            coeff3 = -8.6731532+(129.946172+(-558.971892+(843.967330-409.109529*x)*x)*x)*x
+            x_approx[i][j] = (-3500 / slope[i][j]) * coeff2
+            
+            print(" {} | {} || {} || {} ".format(v1[i],v2[j],f"{int(x1[i][j]):03d}", f"{int(x_approx[i][j]):03d}"))
           
-print("Total number of valid points: ",k)
+# print("Total number of valid points: ",k)
 
-for i in range(0,21):
+for i in range(len(v1)):
     print('{', end='')
-    for j in range(0,21):
-        print(int(x[i][j]), end='')
-        if j != 20:
+    for j in range(len(v1)):
+        print(int(x1[i][j]), end='')
+        if j != len(v1)-1:
             print(',', end='')
     print('},')
+
 plt.show()
